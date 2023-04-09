654aa428a0ca25f8d3bf9cabc0dd0fa2c687bfca
Stanislav Ivanov <standimiv@yahoo.com>
2023-04-09 21:52:33 +0300

    Setting up further basics for the future food ordering website

---
+++ src/main/java/expectations/Ingredient.java
@@ -0,0 +1,15 @@
+package expectations;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+	
+	private String id;
+	private String name;
+	private Type type;
+	
+	public enum Type {
+		DOUGH, SAUCE, MEAT, VEGETABLES, CHEESE, SEASONING  
+	}
+}
---
+++ src/main/java/expectations/Pizza.java
@@ -0,0 +1,13 @@
+package expectations;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Pizza {
+
+	private String name;
+	
+	private List<Ingredient> ingredients;
+}
